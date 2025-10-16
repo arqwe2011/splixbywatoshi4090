aee21a7dabb4e84f146c4128cb390f1bc632edd5
arqwe2011 <arsenqwe2011@gmail.com>
2025-10-17 01:04:52 +0500

    Update Splix Lib Example.lua

--- Splix Lib Example.lua
+++ Splix Lib Example.lua
@@ -1,5 +1,5 @@
 -- // Tables
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/matas3535/PoopLibrary/main/Library.lua"))() -- Could Also Save It In Your Workspace And Do loadfile("Library.lua")()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/arqwe2011/splixbywatoshi4090/refs/heads/main/Library.lua"))() -- Could Also Save It In Your Workspace And Do loadfile("Library.lua")()
 -- // Variables
 -- // Init
 local Window = Library:New({Name = "Splix Private Poop UI", Accent = Color3.fromRGB(25, 240, 100)})
@@ -35,4 +35,5 @@ Settings_Main:ButtonHolder({Buttons = {{"Load", function() end}, {"Save", functi
 Settings_Main:Label({Name = "Unloading will fully unload\neverything, so save your\nconfig before unloading.", Middle = true})
 Settings_Main:Button({Name = "Unload", Callback = function() Window:Unload() end})
 -- // Initialisation
+
 Window:Initialize()
